bb30bea50a5641e07436ef6c07f86c082494ab22
Khanittha40401 <40401@prot.ac.th>
2025-03-11 20:29:26 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,20 @@
+
+def main():
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = num1 * num2
+        
+        if result > 0:
+            print("The result is positive.")
+        elif result < 0:
+            print("The result is negative.")
+        else:
+            print("The result is zero.")
+        
+        print(f"Multiplication result: {result}")
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+
+if __name__ == "__main__":
+    main()
