33949cfbdd98fe68f5062811df33816bce255e79
Khanittha40401 <40401@prot.ac.th>
2025-03-11 20:25:59 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+
+number = float(input("Please enter a number: "))
+
+# ตรวจสอบว่าหมายเลขนั้นเท่ากับศูนย์หรือไม่
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
