045ace08683e23230d5a8d8a8f052f82871165f2
Khanittha40401 <40401@prot.ac.th>
2025-03-13 10:15:58 +0700

    Add files via upload

---
+++ cell05/ex04/paramaters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Get the number of parameters (excluding the script name)
+num_parameters = len(sys.argv) - 1
+
+# Display the number of parameters
+print(num_parameters)
